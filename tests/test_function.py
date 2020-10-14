31da9200106145450d3c66896d895cd7796700d8
Håkon V. Treider <haakonvt@gmail.com>
2020-10-14 13:23:51 +0200

    Salvage tests..

--- tests/test_function.py
+++ tests/test_function.py
@@ -9,8 +9,7 @@ from function import (
     FunctionDeployError,
     FunctionDeployTimeout,
     await_function_deployment,
-    create_and_wait,
-    deploy_function,
+    create_function_and_wait,
     file_exists,
     function_exist,
     get_file_name,
@@ -20,29 +19,27 @@ from function import (
     upload_and_create,
 )
 
+# TODO: Tests need an overhaul / update
+
 
 @pytest.mark.parametrize(
-    "retrieve_status, wait_time_seconds, expected, expectation",
+    "retrieve_status, wait_time_seconds, expectation",
     [
-        (["Ready"], 1, True, contextlib.nullcontext()),
-        (["Failed"], 1, False, pytest.raises(FunctionDeployError)),
-        (["Not ready", "Ready"], 4, True, contextlib.nullcontext()),
-        (["Not ready", "Failed"], 4, False, pytest.raises(FunctionDeployError)),
-        (["Not ready"], 3, False, contextlib.nullcontext()),
+        (["Ready"], 1, contextlib.nullcontext()),
+        (["Failed"], 1, pytest.raises(FunctionDeployError)),
+        (["Not ready", "Ready"], 4, contextlib.nullcontext()),
+        (["Not ready", "Failed"], 4, pytest.raises(FunctionDeployError)),
     ],
 )
 def test_await_function_deployment(
-    retrieve_status, wait_time_seconds, expected, expectation, cognite_experimental_client_mock
+    retrieve_status, wait_time_seconds, expectation, cognite_experimental_client_mock
 ):
     responses = [Function(status=status, error={"trace": "some_error"}) for status in retrieve_status]
     cognite_experimental_client_mock.functions.retrieve.side_effect = responses
     with expectation:
         r = await_function_deployment(cognite_experimental_client_mock, "", wait_time_seconds)
-        if expected:
-            assert r
-            assert r == responses[-1]
-        else:
-            assert r is None
+        assert isinstance(r, Function)
+        assert r == responses[-1]
 
 
 @patch("function.try_delete_function")
@@ -85,23 +82,23 @@ def test_try_delete_function_file(file_exists_mock, cognite_client_mock, exists,
 
 @pytest.mark.parametrize(
     "response, expectation",
-    [(Function(external_id="id", id=1), contextlib.nullcontext()), (None, pytest.raises(FunctionDeployTimeout))],
+    [(Function(external_id="id", id=1), contextlib.nullcontext())],
 )
 @patch("function.await_function_deployment")
 def test_create_and_wait(await_function_deployment_mock, response, expectation, cognite_experimental_client_mock):
     await_function_deployment_mock.return_value = response
-    function = Function(external_id="id")
-    cognite_experimental_client_mock.functions.create.return_value = function
+    mock_external_id = response.external_id
+    cognite_experimental_client_mock.functions.create.return_value = Function(external_id=mock_external_id)
     mock_config = MagicMock()
+    mock_config.external_id = mock_external_id
     mock_config.deploy_wait_time_sec = 1337
     with expectation:
-        assert response == create_and_wait(cognite_experimental_client_mock, "id", mock_config)
-        print(await_function_deployment_mock.call_args_list)
-        assert await_function_deployment_mock.call_args_list == [call(cognite_experimental_client_mock, "id", 1337)]
+        assert response == create_function_and_wait(cognite_experimental_client_mock, mock_external_id, mock_config)
+        assert await_function_deployment_mock.call_args_list == [call(cognite_experimental_client_mock, mock_external_id, 1337)]
 
 
 @pytest.mark.parametrize("exception", [FunctionDeployTimeout, FunctionDeployError])
-@patch("function.create_and_wait")
+@patch("function.create_function_and_wait")
 @patch("function.try_delete_function_file")
 def test_upload_and_create_exception(
     try_delete_function_file_mock,
@@ -114,27 +111,6 @@ def test_upload_and_create_exception(
 
     with pytest.raises(exception):
         upload_and_create(cognite_client_mock, valid_config)
-
-
-@patch("function.try_delete")
-@patch("function.upload_and_create")
-def test_deploy_function_push(upload_and_create_mock, try_delete_mock, cognite_client_mock, valid_config):
-    expected_function = Function()
-    upload_and_create_mock.return_value = expected_function
-
-    result = deploy_function(cognite_client_mock, valid_config)
-    assert result == expected_function
-    assert try_delete_mock.call_args_list == [call(cognite_client_mock, valid_config.external_id)]
-    assert upload_and_create_mock.call_args_list == [call(cognite_client_mock, valid_config)]
-
-
-@patch("function.try_delete")
-@patch("function.upload_and_create")
-def test_function_delete(upload_and_create_mock, try_delete_mock, monkeypatch, cognite_client_mock, valid_config):
-    valid_config.remove_only = True
-    assert deploy_function(cognite_client_mock, valid_config) is None
-    assert try_delete_mock.call_args_list == [call(cognite_client_mock, valid_config.external_id)]
-    assert upload_and_create_mock.call_args_list == []
 
 
 @pytest.mark.parametrize("response, expected", [(Function(), True), (None, False)])
