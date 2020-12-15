e68420c02cf0d0491be51f3530582f3b3a0f4ead
Håkon V. Treider <haakonvt@gmail.com>
2020-12-15 16:52:16 +0100

    Bug: Schedules on pr deployments... (#24)
    * Implementation of if-test skip-schedules
    
    * Removed attach_schedules param in favor of existing

--- src/index.py
+++ src/index.py
@@ -32,6 +32,7 @@ def main(config: FunctionConfig) -> None:
 
 
 def setup_config() -> FunctionConfig:
+    # Note: The "or None" is there so that - if the empty string is passed - we pass None instead.
     return FunctionConfig(
         external_id=os.getenv("INPUT_FUNCTION_NAME", ""),
         folder_path=os.getenv("INPUT_FUNCTION_FOLDER", ""),
--- src/schedule.py
+++ src/schedule.py
@@ -8,12 +8,25 @@ from config import FunctionConfig
 logger = logging.getLogger(__name__)
 
 
+def delete_all_schedules_attached(client: CogniteClient, function: Function):
+    """
+    We want to delete ALL existing schedules since we don't keep state anywhere and we want to wipe:
+      1. Those we are going to recreate
+      2. Those removed permanently
+    """
+    schedule_ids = [schedule.id for schedule in function.list_schedules()]
+    if schedule_ids:
+        for sid in schedule_ids:  # TODO: Experimental SDK does not support "delete multiple"
+            client.functions.schedules.delete(sid)
+        logger.info(f"Deleted all ({len(schedule_ids)}) existing schedule(s)!")
+
+
 def deploy_schedule(client: CogniteClient, function: Function, config: FunctionConfig):
-    for schedule in function.list_schedules():
-        # We want to delete ALL existing schedules since we don't keep state anywhere and we want to wipe:
-        # 1. Those we are going to recreate
-        # 2. Those removed permanently
-        client.functions.schedules.delete(schedule.id)
+    delete_all_schedules_attached(client, function)
+
+    if not config.schedules:
+        logger.info("Skipped step of attaching schedules!")
+        return
 
     for schedule in config.schedules:
         client.functions.schedules.create(
