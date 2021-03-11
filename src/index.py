364c61be74645a0c061d1154f0f78b7e997b4746
Håkon V. Treider <haakonvt@gmail.com>
2021-03-11 15:39:49 +0100

    Update index.py

--- src/index.py
+++ src/index.py
@@ -34,7 +34,7 @@ def main(config: FunctionConfig) -> None:
 def setup_config() -> FunctionConfig:
     for k, v in os.environ.items():
         if k.startswith("INPUT"):
-            print(k, v)
+            logger.info(k, v)
     # Note: The "or None" is there so that - if the empty string is passed - we pass None instead.
     return FunctionConfig(
         external_id=os.getenv("INPUT_FUNCTION_NAME", ""),
