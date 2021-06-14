2063e8e4b4d6291835fffd32c7bc374bd5ee2359
Håkon V. Treider <haakonvt@gmail.com>
2021-06-14 14:20:58 +0200

    Add AST check for handle args

---
+++ src/checks.py
@@ -0,0 +1,34 @@
+import ast
+import contextlib
+import logging
+
+from config import FunctionConfig
+
+logger = logging.getLogger(__name__)
+
+
+HANDLE_ARGS = ("data", "client", "secrets", "function_call_info")
+
+
+def run_checks(config: FunctionConfig) -> None:
+    # Python-only checks:
+    if config.function_file.endswith(".py"):
+        _check_handle_args(config.function_file)
+
+
+def _check_handle_args(filename: str, fn_name: str = "handle") -> None:
+    with contextlib.suppress(FileNotFoundError):
+        with open(filename) as file:
+            file = file.read()
+        for node in ast.walk(ast.parse(file)):
+            if isinstance(node, ast.FunctionDef) and node.name == fn_name:
+                bad_args = set(param.arg for param in node.args.args).difference(HANDLE_ARGS)
+                if not bad_args:
+                    return
+                err_msg = (
+                    f"In file '{file}', function '{fn_name}' contained illegal args: {list(bad_args)}. "
+                    f"The function args must be a strict subset of: {list(HANDLE_ARGS)} "
+                    "(ordering is not important)"
+                )
+                logger.error(err_msg)
+                raise ValueError(err_msg)
